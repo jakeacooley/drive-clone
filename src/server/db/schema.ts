f732580b8a7718925ec1e83d29970695f70e5cde
Jake Cooley <jakeacooley@gmail.com>
2025-03-18 00:54:47 -0700

    update schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,7 +1,25 @@
-import { int, text, singlestoreTable, bigint } from "drizzle-orm/singlestore-core";
+import { int, text, bigint, index, singlestoreTableCreator } from "drizzle-orm/singlestore-core";
 
-export const users = singlestoreTable("users_table", {
-  id: bigint("id", { mode: "bigint" }).primaryKey().autoincrement(),
-  name: text("name"),
-  age: int("age"),
+export const createTable = singlestoreTableCreator((name) => `drive-clone_${name}`);
+
+export const files = createTable("files_table", {
+  id: bigint("id", { mode: "bigint", unsigned: true }).primaryKey().autoincrement(),
+  name: text("name").notNull(),
+  url: text("url").notNull(),
+  parent: bigint("parent", { mode: "bigint", unsigned: true }).notNull(),
+  size: int("size").notNull(),
+}, (t) => {
+  return [
+    index("parent_index").on(t.parent),
+  ];
+});
+
+export const folders = createTable("folders_table", {
+  id: bigint("id", { mode: "bigint", unsigned: true }).primaryKey().autoincrement(),
+  name: text("name").notNull(),
+  parent: bigint("parent", { mode: "bigint", unsigned: true }),
+}, (t) => {
+  return [
+    index("parent_index").on(t.parent),
+  ];
 });
